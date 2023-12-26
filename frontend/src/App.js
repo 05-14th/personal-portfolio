83b70d3dc3f5ea0e637188074bab91174b2dbb8b
Gerry Vien Flores <gerryvienlifeflores@gmail.com>
2023-12-26 11:27:52 +0800

    Readd

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="bg-gray-200 p-4">
+      <h1 className="text-3xl font-bold text-center">Welcome to my Tailwind CSS App</h1>
+      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4">
+        Click me
+      </button>
+    </div>
+  );
+}
+
+export default App;
