80b636e5c1ef3b7c45a441e92f1284df919c062d
Gerry Vien Flores <gerryvienlifeflores@gmail.com>
2023-12-26 19:46:36 +0800

    Front Page 50%

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,11 +2,25 @@ import './App.css';
 
 function App() {
   return (
-    <div className="bg-gray-200 p-4">
-      <h1 className="text-3xl font-bold text-center">Welcome to my Tailwind CSS App</h1>
-      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4">
-        Click me
-      </button>
+    <div className="bg-gray-200 flex h-screen">
+      <div className="bg-white w-2/4 flex justify-center items-center">
+        <div className="w-3/4">
+          <h2 className="font-bold font-sans text-4xl">HI THERE!</h2>
+          <h1 className="font-serif text-5xl"><b>I'M</b> GERRY VIEN</h1>
+          <div className="bg-yellow-500">
+            <h4 className="font-sans font-bold text-white text-center">FULL STACK WEB AND SOFTWARE DEVELOPER</h4>
+          </div>
+          <p className="text-justify">
+            I'm Gerry Vien Flores, a dedicated software and web developer passionate about continuous learning in tech. I thrive on challenges, from tech competitions to commissioned projects, seeing them not just as hobbies but integral to my career growth. My proactive approach ensures I stay adept in the ever-evolving tech landscape.
+          </p>
+          <button className="bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-2 px-4 rounded mt-4">
+            MORE ABOUT ME
+          </button>
+        </div>
+      </div>
+      <div className='w-2/4'>
+        <h1>Sample</h1>
+      </div>
     </div>
   );
 }
