c7fd783bc07f51abb7578397198af7a3a637d8ba
Gerry Vien Flores <gerryvienlifeflores@gmail.com>
2023-12-28 15:21:11 +0800

    Latest Update

--- frontend/src/About.js
+++ frontend/src/About.js
@@ -2,7 +2,6 @@ import './App.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHome, faUser, faBriefcase, faImage, faFolder, faContactCard } from '@fortawesome/free-solid-svg-icons';
 import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
-import About from './About'; // Import your About component
 
 function App() {
 
@@ -14,28 +13,16 @@ function App() {
   return (
     <Router>
       <div className="bg-gray-200 flex h-screen">
-        <div className="bg-white w-3/5 flex justify-center items-center">
-          <div className="w-3/4 max-h-full overflow-y-auto">
-            <h2 className="font-bold font-sans text-4xl">HI THERE!</h2>
-            <h1 className="font-serif text-5xl"><b>I'M</b> GERRY VIEN</h1>
-            <div className="bg-yellow-500">
-              <h4 className="font-sans font-bold text-white text-center">FULL STACK WEB AND SOFTWARE DEVELOPER</h4>
-            </div>
-            <p className="text-justify">
-              I'm Gerry Vien Flores, a dedicated software and web developer passionate about continuous learning in tech. I thrive on challenges, from tech competitions to commissioned projects, seeing them not just as hobbies but integral to my career growth. My proactive approach ensures I stay adept in the ever-evolving tech landscape.
-            </p>
-            <button className="bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-2 px-4 rounded mt-4" onClick={handleClick}>
-              MORE ABOUT ME
-            </button>
-          </div>
+        <div className="bg-white w-1/5 flex justify-center items-center">
+          
         </div>
-        <div className="w-2/5 background flex justify-end items-center">
+        <div className="w-4/5 background flex justify-end items-center">
           <ul className="m-4 bg-yellow-500 p-2 rounded-md">
             <li>
               <Link to="/" className="text-white"><FontAwesomeIcon icon={faHome} size="lg"/></Link>
             </li>
             <li>
-              <Link to='/about' className='hover:text-white'><FontAwesomeIcon icon={faUser} size="lg"/></Link>
+              <Link to='/' className='hover:text-white'><FontAwesomeIcon icon={faUser} size="lg"/></Link>
             </li>
             <li>
               <Link to='/' className='hover:text-white'><FontAwesomeIcon icon={faBriefcase}  size="lg"/></Link>
@@ -52,10 +39,6 @@ function App() {
           </ul>
         </div>
       </div>
-
-      <Switch>
-        <Route path="/about" component={About} />
-      </Switch>
     </Router>
   );
 }
